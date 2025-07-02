71d2ab7e73b5a52ee2d41fee7ab6c33d1ab3b6c8
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-07-01 21:26:39 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,29 @@ import Tarjetas from './componentes/Tarjetas';
 import Alertas from './componentes/Alertas';
 import Footer from './componentes/Footer';
 import { Container } from '@mui/material';
+import imagen1 from '../src/assets/1060-800x300.jpg';
+import imagen2 from '../src/assets/1065-800x300.jpg';
+  import imagen3 from '../src/assets/1071-800x300.jpg';
+
+const images = [imagen1, imagen2, imagen3];
+
 
 function App(): JSX.Element {
   return (
     <>
+      <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/nueva-categoria" element={<NuevaCategoria />} />
+        <Route path="/calculos" element={<Calculos />} />
+        <Route path="/informacion" element={<Informacion />} />
+      </Routes>
+    </Router>
       <Container sx={{ mt: 4 }}>
         <Bienvenida />
-        <Carrusel />
+        <Carrusel images={images} />
         <Tarjetas />
         <Alertas />
       </Container>
--- src/componentes/Carrusel.tsx
+++ src/componentes/Carrusel.tsx
@@ -1,30 +1,59 @@
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Autoplay from 'swiper';              // Importa el módulo Autoplay
-import { Box } from '@mui/material';
-import imagen1 from '../assets/1060-800x300.jpg';
-import imagen2 from '../assets/1065-800x300.jpg';
-import imagen3 from '../assets/1071-800x300.jpg';
+import React, { useState } from 'react';
 
-const Carrusel = () => {
-  const images = [imagen1, imagen2, imagen3];
+interface Carrusel {
+  images: string[];
+}
+
+const Carrusel: React.FC<Carrusel> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleDotClick = (index: number) => {
+    setCurrentIndex(index);
+  };
 
   return (
-    <Box sx={{ mb: 4 }}>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        modules={Autoplay}                  
-        autoplay={{ delay: 2500, disableOnInteraction: false }}  
-        loop={true}                           
-      >
-        {images.map((src, i) => (
-          <SwiperSlide key={i}>
-            <img src={src} alt={`slide-${i}`} style={{ width: '100%' }} />
-          </SwiperSlide>
+    <div className="Carrusel">
+      <img
+        key={currentIndex}
+        src={images[currentIndex]}
+        alt={`slide-${currentIndex}`}
+      />
+      <div className="slide_direction">
+        <div className="left" onClick={handlePrevious}>
+          {/* SVG izquierda */}
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+          </svg>
+        </div>
+        <div className="right" onClick={handleNext}>
+          {/* SVG derecha */}
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+          </svg>
+        </div>
+      </div>
+      <div className="indicator">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${currentIndex === index ? "active" : ""}`}
+            onClick={() => handleDotClick(index)}
+          />
         ))}
-      </Swiper>
-    </Box>
+      </div>
+    </div>
   );
 };
 
